69467f549bfbe4a7cf692912e7c85a28dc5560e9
Himanshukaloni <himanshukaloni99@gmail.com>
2026-01-14 19:37:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// simple form handler
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  alert("Message sent successfully!");
+});
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  let phoneNumber = "917452083547"; // YOUR WhatsApp number (no +)
+
+  let whatsappMessage =
+    `Hello Himanshu,%0A%0A` +
+    `Name: ${name}%0A` +
+    `Email: ${email}%0A` +
+    `Message: ${message}`;
+
+  let whatsappURL =
+    `https://wa.me/${phoneNumber}?text=${whatsappMessage}`;
+
+  window.open(whatsappURL, "_blank");
+});
+
